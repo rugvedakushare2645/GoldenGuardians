50aabf580fd44ca52e9f7300babf06b66b692759
rugveda <rugvedakushare2645@gmail.com>
2024-03-11 20:44:50 +0530

    made changes to services page

--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -1,12 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
 
 namespace GoldenGuardians.Pages
 {
     public class LoginModel : PageModel
-    {
-        public void OnGet()
-        {
-        }
-    }
+	{
+		[BindProperty]
+		public required LoginModel log { get; set; }
+		public void OnPost()
+		{
+			string email = Request.Form["email"];
+			string password = Request.Form["pass"];
+
+			using (MySqlConnection con = new MySqlConnection("server=localhost;username=root;database=gyg;port=3306;password=sau@271202"))
+			{
+				//opening connection
+				con.Open();
+
+				string query = "select * from log where email = ?email;";
+
+				MySqlCommand cmd = new MySqlCommand(query, con);
+
+				con.Close();
+			}
+		}
+	}
 }
+
